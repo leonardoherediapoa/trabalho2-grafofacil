52ce7fc95980a0a73b6dd492bcbb1746b10ab0c2
Rodrigo Gagliardi <rodrigogagliardi18@gmail.com>
2025-06-12 23:19:04 -0300

    sory

--- src/algoritmos/BuscaProfundidade.java
+++ src/algoritmos/BuscaProfundidade.java
@@ -1,4 +1,21 @@
 package algoritmos;
 
+import estruturas.*;
+
 public class BuscaProfundidade {
+    private Grafo grafo;
+    private int origem;
+    private boolean[] percorridos;
+    private int[] anteriores;
+
+    public BuscaProfundidade(Grafo g, int origem) {
+        this.grafo = g;
+        this.origem = origem;
+        percorridos = new boolean[g.getNumeroVertices()];
+        anteriores = new int[g.getNumeroVertices()];
+        for(int i = 0; i<anteriores.length; i++) anteriores[i] = -1;
+        buscarEmProfundidadeRecursivo(this.grafo, this.origem);
+    }
+
+
 }
