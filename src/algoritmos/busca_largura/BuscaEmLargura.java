f147f492581037ae66c0b021d4b33673e70bd790
Daniel <daniel.glasenapp@edu.pucrs.br>
2025-06-15 15:41:19 -0300

    Versao atualizada sem utilizar Wrapper 'Boolean'

--- src/algoritmos/busca_largura/BuscaEmLargura.java
+++ src/algoritmos/busca_largura/BuscaEmLargura.java
@@ -7,58 +7,56 @@ import estruturas.Vertice;
 import java.util.*;
 
 public class BuscaEmLargura {
-    private Map<Vertice, Boolean> visitado;
+    private Set<Vertice> visitado;
     private Map<Vertice, Vertice> anterior;
 
     public BuscaEmLargura(Grafo grafo, Vertice origem) {
-        visitado = new HashMap<>();
+        visitado = new HashSet<>();
         anterior = new HashMap<>();
 
-        for (Vertice v : grafo.getListaVertices()) {
-            visitado.put(v, false);
-            anterior.put(v, null);
-        }
-
         realizarBusca(grafo, origem);
-    }
-
-    public List<Vertice> caminhoAte(Vertice destino) {
-        List<Vertice> caminho = new ArrayList<>();
-
-        Vertice atual = destino;
-        while (atual != null) {
-            caminho.add(0, atual); 
-            atual = anterior.get(atual);
-        }
-
-        return caminho;
     }
 
     private void realizarBusca(Grafo grafo, Vertice origem) {
         Queue<Vertice> fila = new LinkedList<>();
         fila.add(origem);
-        visitado.put(origem, true);
+        visitado.add(origem);
+        anterior.put(origem, null);
 
         while (!fila.isEmpty()) {
-            Vertice atual = fila.poll(); 
+            Vertice atual = fila.poll();
 
-
-            List<Aresta> arestas = grafo.getAdjacencias(atual);
-
-            for (Aresta aresta : arestas) {
+            for (Aresta aresta : grafo.getAdjacencias(atual)) {
                 Vertice vizinho;
+
                 if (aresta.getOrigem().equals(atual)) {
                     vizinho = aresta.getDestino();
                 } else {
                     vizinho = aresta.getOrigem();
                 }
 
-                if (!visitado.get(vizinho)) {
-                    visitado.put(vizinho, true);
+                if (!visitado.contains(vizinho)) {
+                    visitado.add(vizinho);
                     anterior.put(vizinho, atual);
                     fila.add(vizinho);
                 }
             }
         }
+    }
+
+    public List<Vertice> caminhoAte(Vertice destino) {
+        List<Vertice> caminho = new ArrayList<>();
+
+        if (!visitado.contains(destino)) {
+            return caminho;
+        }
+
+        Vertice atual = destino;
+        while (atual != null) {
+            caminho.add(0, atual);
+            atual = anterior.get(atual);
+        }
+
+        return caminho;
     }
 }
